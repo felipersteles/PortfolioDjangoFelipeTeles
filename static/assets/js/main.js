309ff7d0d0f6a94bd7f50499598ec731d41f8e2f
Felipe Teles <felipersteles@gmail.com>
2022-09-19 22:38:29 -0300

    scrool correction

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -9,15 +9,17 @@ const showMenu = (toggleId, navId) =>{
         })
     }
 }
+
 showMenu('nav-toggle','nav-menu')
 
-/*===== REMOVE MENU MOBILE =====*/
+/*===== desabilitar menu no mobile =====*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
